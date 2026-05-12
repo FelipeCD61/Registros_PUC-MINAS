c0425f35d6b73cb47114911b545d486b58692355
FelipeCD61 <127131423+FelipeCD61@users.noreply.github.com>
2026-05-12 08:47:36 -0300

    setup inicial do frontend em react e configuracao do axios

---
+++ frontend/todo-app-web/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Todo App do Felipe</h1>
+      <p>O Frontend React está pronto e configurado!</p>
+    </div>
+  );
+}
+
+export default App;
